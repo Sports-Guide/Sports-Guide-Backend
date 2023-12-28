a5131eb079869d3e59eaff052bbc527ab0a4281b
Симонов Алексей <alxxalxx@mail.ru>
2023-12-28 16:44:56 +0300

    mv manager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,11 +1,34 @@
 from django.db import models
 
 from django.core.validators import MinLengthValidator
-from .validators import validate_password
 
 from django.contrib.auth.models import AbstractBaseUser
-from .manager import CustomUserManager
 from django.contrib.auth.models import PermissionsMixin
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("Email должен быть указан"))
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, nickname, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(nickname, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
@@ -26,21 +49,16 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         blank=True,
         null=True
     )
-    password = models.CharField(
-        'Пароль',
-        validators=[validate_password],
-        max_length=25
-    )
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD = 'nickname'
+    USERNAME_FIELD = 'email'
 
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
     def __str__(self):
-        return f'{self.nickname}'
+        return f'{self.email}'
