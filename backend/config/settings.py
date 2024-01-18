d0424eebf311a0165f28c29446832054a48d4b2f
Владислав Суворов <gordik_08@mail.ru>
2024-01-18 17:57:35 +0300

    Add documentation, fixed linter errors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,19 +85,23 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'users.validators.CustomPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
         'OPTIONS': {
             'min_length': 6,
         }
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
     },
 ]
 
